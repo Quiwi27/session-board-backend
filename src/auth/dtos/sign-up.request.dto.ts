2e5a0f159b179167e9f5e380f04d142cc13549f3
Quiwi27 <dev.rostyslav.liashenko@gmail.com>
2026-01-11 00:47:40 +0200

    feat: add validation

--- src/auth/dtos/sign-up.request.dto.ts
+++ src/auth/dtos/sign-up.request.dto.ts
@@ -1,5 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { Transform } from 'class-transformer';
+
 export class SignUpRequestDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @Transform(({ value }: { value: string }) => value?.toLowerCase())
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
   password: string;
 }
--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -3,6 +3,6 @@ import { pgTable, uuid, varchar } from 'drizzle-orm/pg-core';
 export const userTable = pgTable('users', {
   id: uuid().primaryKey().defaultRandom(),
   name: varchar().notNull(),
-  email: varchar().notNull(),
+  email: varchar().notNull().unique(),
   password: varchar().notNull(),
 });
