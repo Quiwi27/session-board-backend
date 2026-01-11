91c93f32e9c3a4992473c4ee8947697786c1b2fe
Quiwi27 <dev.rostyslav.liashenko@gmail.com>
2026-01-11 19:49:23 +0200

    feat: add endpoint to join user

--- src/sessions/controllers/game-session.controller.ts
+++ src/sessions/controllers/game-session.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { GameSessionService } from '../services/game-session.service';
 import { CreateSessionOmitCreatorRequestDto, CreateSessionRequestDto } from '../dtos/requests/create-session.request.dto';
 import { SessionResponseDto } from '../dtos/responses/session.response.dto';
 import { GetDecodedUser } from 'src/auth/decorators/get-decoded-user.decorator';
 import { DecodedJwtUserDto } from 'src/auth/dtos/decoded-jwt-user.dto';
+import { ParticipantResponseDto } from '../dtos/responses/participant.reponse.dto';
 
 @Controller({
   version: '1',
@@ -20,5 +21,10 @@ export class GameSessionConroller {
     const createSessionDto: CreateSessionRequestDto = { ...requestDto, creatorId: user.userId };
 
     return this.gameSessionService.createSession(createSessionDto);
+  }
+
+  @Post(':sessionId/join')
+  public join(@Param('sessionId') sessionId: string, @GetDecodedUser() user: DecodedJwtUserDto): Promise<ParticipantResponseDto> {
+    return this.gameSessionService.joinSession(sessionId, user.userId);
   }
 }
